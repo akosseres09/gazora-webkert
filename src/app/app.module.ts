326ba259f27b98089f36fa71e0eb521bbe43a8cc
Seres Ákos <akosseres75@gmail.com>
2024-05-04 11:28:08 +0200

    signup and login with toolbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,19 +8,39 @@ import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
 import { getAuth, provideAuth } from '@angular/fire/auth';
 import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 import { getStorage, provideStorage } from '@angular/fire/storage';
+import { ToolbarMenuComponent } from './shared/toolbar-menu/toolbar-menu.component';
+import { MatSidenavModule } from "@angular/material/sidenav";
+import { MatToolbarModule } from "@angular/material/toolbar";
+import { MatIconModule } from "@angular/material/icon";
+import { MatButtonModule } from "@angular/material/button";
+import {MatListModule} from "@angular/material/list";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ToolbarMenuComponent,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    provideFirebaseApp(() => initializeApp({"projectId":"gazora-webkert-3ccf4","appId":"1:579222676333:web:7fd65ab5f9982d7330b064","storageBucket":"gazora-webkert-3ccf4.appspot.com","apiKey":"AIzaSyBUiKGuqA8_zUEpYlwDg69G5ct9oZaGnhM","authDomain":"gazora-webkert-3ccf4.firebaseapp.com","messagingSenderId":"579222676333","measurementId":"G-VR72SDVHD5"})),
-    provideAuth(() => getAuth()),
-    provideFirestore(() => getFirestore()),
-    provideStorage(() => getStorage())
-  ],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        provideFirebaseApp(() => initializeApp({
+            "projectId": "gazora-webkert-3ccf4",
+            "appId": "1:579222676333:web:7fd65ab5f9982d7330b064",
+            "storageBucket": "gazora-webkert-3ccf4.appspot.com",
+            "apiKey": "AIzaSyBUiKGuqA8_zUEpYlwDg69G5ct9oZaGnhM",
+            "authDomain": "gazora-webkert-3ccf4.firebaseapp.com",
+            "messagingSenderId": "579222676333",
+            "measurementId": "G-VR72SDVHD5"
+        })),
+        provideAuth(() => getAuth()),
+        provideFirestore(() => getFirestore()),
+        provideStorage(() => getStorage()),
+        MatIconModule,
+        MatToolbarModule,
+        MatSidenavModule,
+        MatButtonModule,
+        MatListModule
+    ],
   providers: [
     provideAnimationsAsync()
   ],
---
+++ src/app/shared/toolbar-menu/toolbar-menu.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-toolbar-menu',
+  templateUrl: './toolbar-menu.component.html',
+  styleUrl: './toolbar-menu.component.css'
+})
+export class ToolbarMenuComponent {
+
+}
