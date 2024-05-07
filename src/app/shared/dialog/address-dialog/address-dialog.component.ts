c4a0edfeaab64455d020567e57f0a92008e7515d
Seres Ákos <akosseres75@gmail.com>
2024-05-08 00:08:26 +0200

    dialogs

---
+++ src/app/shared/dialog/address-dialog/address-dialog.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-address-dialog',
+  templateUrl: './address-dialog.component.html',
+  styleUrl: './address-dialog.component.css'
+})
+export class AddressDialogComponent {
+
+}
